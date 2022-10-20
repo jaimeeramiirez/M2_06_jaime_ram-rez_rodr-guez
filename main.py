023b8d62aeb545528ffb861944cdd55feefe903e
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-20 18:16:10 +0000

    HE CAMBIADO LOS COMENTARIOS DEL EJ.2

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ area_circulo()
 #EJERCICIO 2
 print("\n\n")
 
-#Creamos la función lee_numero, dentro de la fución. Dentro de la función número crearemos una función llamada "mayor" que nos buscará el numero mayor de los tres introducidos; los número los introduciremos con la función mayor y buscará el número MÁXIMO con la función MAX.
+#Creamos la función lee_numero con la varibale "num" que nos pedirá que introduzcamos un número. Las funciones las utilizamos para ordenar y ahorrar código por lo que con esta función en vez de escribir tres veces: num=float(input("Introduzca un número: ")) solo tendremos que hacer referencia a la función en la otra función que llamaremos "mayor". Esta función buscará el numero mayor de los tres introducidos; para ellor usaremos la herramienta MAX.
 def lee_numero():
   num=float(input("Introduzca un número: "))
   return num
