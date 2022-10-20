4adc86b8ef839b200547f13068e51bcde4efcd3e
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-20 18:17:09 +0000

    ejercicio 2 copiado en las pestañas

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,27 +1,16 @@
 #EJERCICIO 2
 print("\n\n")
 
-#Creamos la función lee_numero, dentro de la fución, le diremos al usuario que introduzca tres números que se imprimirán por pantalla en una tabla. Dentro de la función número crearemos una función llamada "mayor" que nos buscará el numero mayor de los tres introducidos. Para ellos usaremos dos condicionales que nos parametrizan la función. Por último, imprimiremos por pantalla el numero mayor.
+#Creamos la función lee_numero con la varibale "num" que nos pedirá que introduzcamos un número. Las funciones las utilizamos para ordenar y ahorrar código por lo que con esta función en vez de escribir tres veces: num=float(input("Introduzca un número: ")) solo tendremos que hacer referencia a la función en la otra función que llamaremos "mayor". Esta función buscará el numero mayor de los tres introducidos; para ellor usaremos la herramienta MAX.
 def lee_numero():
-  print("Introduce tres números diferentes\n")
-  num1=float(input("Introduzca el primer número: "))
-  num2=float(input("Introduzca el segundo número: "))
-  num3=float(input("Introduzca el tercer número: "))
-  lista=[num1, num2, num3]
-  print(lista)
-  print("\n")
-  def mayor():
-    if num1>num2 and num1>num3:
-      num_mayor=num1
-    elif num2>num1 and num2>num3:
-      num_mayor=num2
-    elif num3>num1 and num3>num2:
-      num_mayor=num3
-    elif num1==num2==num3:
-      print("ERROR")
-      
-    print("El número más grande es: ", num_mayor)
+  num=float(input("Introduzca un número: "))
+  return num
+def mayor():
+    num1= lee_numero()
+    num2= lee_numero()
+    num3= lee_numero()
+    MAX=max(num1, num2, num3)
+    print("El número más grande es: ", MAX)
     return mayor
-  mayor()
 
-lee_numero()
+mayor()
